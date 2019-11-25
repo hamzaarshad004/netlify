984f6f27d7e7ab8dde5875106c23948e27c5a2c4
Hamza Arshad <hamzaarshad004@gmail.com>
2019-11-25 22:35:13 +0500

    ju

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import { render } from "react-dom";
 import Routes from "../src/Routes/routes";
 import { ThemeProvider } from "@material-ui/styles";
 
-render(
-  <ThemeProvider theme={Themes.default}>
-    <Routes />
-  </ThemeProvider>,
-  document.getElementById("root")
-);
+render(<Routes />, document.getElementById("root"));
 
 // unregister() to register() below. Note this comes with some pitfalls.
